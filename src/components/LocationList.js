f2b8a09c2bece5be795025b9948579ceb1cb3f65
Anna <annanowaczykmail@gmail.com>
2018-08-03 23:11:31 +0200

    Set render function in LocationList component

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -2,7 +2,9 @@ import React, { Component} from 'react'
 import LocationItem from './LocationItem'
 
 class LocationList extends Component {
-  /*Constructor*/
+  /*
+  Constructor function
+  */
   constructor(props) {
     super(props);
     this.state = {
@@ -11,7 +13,65 @@ class LocationList extends Component {
       'suggestions': true
     };
     this.filterLocations = this.filterLocations.bind(this);
-    this.toggleSuggestions - this.toggleSuggestions.bind(this);
   }
-  
+  /*
+  Filter locations based on user query
+  */
+  filterLocations(event) {
+    this.props.closeInfoWindow();
+    const {value} = event.target;
+    var locations = [];
+    this.props.alllocations.forEach(function (location) {
+      if (location.longname.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        location.marker.setVisible(true);
+        locations.push(location);
+      } else {
+        location.marker.setVisible(false);
+      }
+    });
+
+    this.setState({
+    'locations' = locations,
+    'query' = value
+    });
+  }
+
+  componentWillMount() {
+    this.setState({
+      'locations': this.props.alllocations
+    });
+  }
+  /*
+  Render function of LocationList
+  */
+  render () {
+    var locationlist = this.state.locations.map(function (listItem, index) {
+      return {
+        <LocationItem
+          key={index}
+          openInfoWindow={this.props.openInfoWindow.bind(this)}
+          data={listItem}
+        />
+      );
+    }, this);
+      return
+      <div className="search">
+        <input
+          role="search"
+          aria-labelledby="filter"
+          id="search-field"
+          className="search-field"
+          type="text"
+          placeholder="Filter"
+          value={this.state.query}
+          onChange={this.filterLocations}
+        />
+        <ul className="location-list">
+          {this.state.suggestions && locationlist}
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default LocationList;
