5f68a01d3f8933976a4eebaf237b2c449376d7c1
Anna <annanowaczykmail@gmail.com>
2018-08-04 23:54:18 +0200

    Add Google Maps API key

--- src/App.js
+++ src/App.js
@@ -9,14 +9,33 @@ class App extends Component {
     super(props);
     this.state = {
       /*
-      Gets locations from JSON file
+      Get locations from JSON file
       */
       alllocations: require("MyLocations.JSON"),
       map: '',
       infowindow: '',
       prevmarker: ''
     };
-    
+    /*
+    Retain object instance when used in the function
+    */
+    this.initMap = this.initMap.bind(this);
+    this.openInfoWindow = this.openInfoWindow.bind(this);
+    this.closeInfoWindow - this.closeInfoWindow.bind(this);
+  }
+
+  componentDidMount() {
+    /*
+    Connect the initMap funtion to the global window context in order for Google Maps to invoke it
+    */
+    window.initMap = this.initMap;
+    /*
+    Loading the Google Maps script asynchronously
+    */
+    loadMapJS(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDcw4pl3Rbm5HIL9kiMkOYKJ31jNVq0RqE&callback=initMap"
+    )
+  );
   }
 
   render() {
