175af089bae45dc5d9865fdfbe1e993d3779a8a0
Anna <annanowaczykmail@gmail.com>
2018-08-05 01:46:42 +0200

    Set export for App

--- src/App.js
+++ src/App.js
@@ -167,46 +167,25 @@ class App extends Component {
         });
         this.state.infowindow.close();
       }
-
-      
-
-
-
-
-
-
+      /*
+      Render function od App
+      */
+      render() {
+        return (
+          <div>
+            <LocationList
+              key='100'
+              alllocations={this.state.alllocations}
+              openInfoWindow={this.openInfoWindow}
+              closeInfoWindow={this.closeInfoWindow}
+            />
+            <div id="map">
+            </div>
+          </div>
+          );
         }
-
-
-
-      )
-
-
-
-
-
-
-
       }
 
+      export default App;
 
-
-
-
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+      
